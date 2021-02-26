92cb947212ec96ff09932e202d96538f2fd7b5a4
npes <npesdk@gmail.com>
2021-02-26 10:08:55 +0100

    trying intent builder

--- __init__.py
+++ __init__.py
@@ -6,7 +6,7 @@ class RpiGpio(MycroftSkill):
     def __init__(self):
         MycroftSkill.__init__(self)
 
-    @intent_file_handler('gpio1.rpi.intent')
+    @intent_file_handler('gpio.rpi.intent')
     def handle_gpio_rpi(self, message):
         self.speak_dialog('gpio.rpi')
 
