949e18288d0a56c7b825ad28d1d60df8bf0549a5
npes <npesdk@gmail.com>
2021-02-26 11:12:29 +0100

    playint with intent words

--- __init__.py
+++ __init__.py
@@ -14,36 +14,65 @@ class RpiGpio(MycroftSkill):
     def handle_gpio(self, message):
         pin = message.data.get('pin')
         command = message.data.get('command')
-        if pin == 'gpio one':
-            if command == 'on':
+        # if pin == 'gpio one':
+        #     if command == 'on':
+        #         self.speak_dialog('gpio one is on')
+        #     elif command == 'off':
+        #         self.speak_dialog('gpio one is off')
+        # elif pin == 'gpio two':
+        #     if command == 'on':
+        #         self.speak_dialog('gpio two is on')
+        #     elif command == 'off':
+        #         self.speak_dialog('gpio two is off')
+        # elif pin == 'gpio three':
+        #     if command == 'on':
+        #         self.speak_dialog('gpio three is on')
+        #     elif command == 'off':
+        #         self.speak_dialog('gpio three is off')
+        # elif pin == 'gpio 1':
+        #     if command == 'on':
+        #         self.speak_dialog('gpio one is on')
+        #     elif command == 'off':
+        #         self.speak_dialog('gpio one is off')
+        # elif pin == 'gpio 2':
+        #     if command == 'on':
+        #         self.speak_dialog('gpio two is on')
+        #     elif command == 'off':
+        #         self.speak_dialog('gpio two is off')
+        # elif pin == 'gpio 3':
+        #     if command == 'on':
+        #         self.speak_dialog('gpio three is on')
+        #     elif command == 'off':
+        #         self.speak_dialog('gpio three is off')
+
+        if command == 'on':
+            if pin == 'gpio one':
                 self.speak_dialog('gpio one is on')
-            elif command == 'off':
-                self.speak_dialog('gpio one is off')
-        elif pin == 'gpio two':
-            if command == 'on':
+            elif pin == 'gpio two':
                 self.speak_dialog('gpio two is on')
-            elif command == 'off':
-                self.speak_dialog('gpio two is off')
-        elif pin == 'gpio three':
-            if command == 'on':
+            elif pin == 'gpio three':
                 self.speak_dialog('gpio three is on')
-            elif command == 'off':
-                self.speak_dialog('gpio three is off')
-        elif pin == 'gpio 1':
-            if command == 'on':
+            if pin == 'gpio 1':
                 self.speak_dialog('gpio one is on')
-            elif command == 'off':
-                self.speak_dialog('gpio one is off')
-        elif pin == 'gpio 2':
-            if command == 'on':
+            elif pin == 'gpio 2':
                 self.speak_dialog('gpio two is on')
-            elif command == 'off':
-                self.speak_dialog('gpio two is off')
-        elif pin == 'gpio 3':
-            if command == 'on':
+            elif pin == 'gpio 3':
                 self.speak_dialog('gpio three is on')
-            elif command == 'off':
+        elif command == 'off':
+            if pin == 'gpio one':
+                self.speak_dialog('gpio one is off')
+            elif pin == 'gpio two':
+                self.speak_dialog('gpio two is off')
+            elif pin == 'gpio three':
                 self.speak_dialog('gpio three is off')
+            if pin == 'gpio 1':
+                self.speak_dialog('gpio one is off')
+            elif pin == 'gpio 2':
+                self.speak_dialog('gpio two is off')
+            elif pin == 'gpio 3':
+                self.speak_dialog('gpio three is off')
+        else:
+            self.speak_dialog('i do not understand you, please try again')
 
 
 def create_skill():
